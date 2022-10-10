701e3fed9818ef8ebcdc8e0885e97b6071c47dcb
chumanhtien <tiencm194182@gmail.com>
2022-10-11 00:35:28 +0700

    FE_1_Restaurant_Food: header section and search bar

--- FE1_Restaurant_Food/js/app.js
+++ FE1_Restaurant_Food/js/app.js
@@ -1,7 +1,23 @@
 let menu = document.querySelector('#menu-bars')
 let navbar = document.querySelector('.navbar')
+let searchIcon = document.querySelector('#search-icon')
+let searchForm = document.querySelector('#search-form')
+let closeSearchForm = document.querySelector('#close')
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times')
     navbar.classList.toggle('active')
+})
+
+window.onscroll = () => {
+    menu.classList.toggle('fa-times')
+    navbar.classList.toggle('active')
+}
+
+searchIcon.addEventListener('click', function () {
+    searchForm.classList.toggle('active')
+})
+
+closeSearchForm.addEventListener('click', function () {
+    searchForm.classList.remove('active')
 })
